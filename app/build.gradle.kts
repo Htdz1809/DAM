f931a950601ffd543b1be59e32b2a2a85eb6e185
hasijsk <hadtoud003@gmail.com>
2025-04-02 14:55:38 +0100

    update v 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,14 +29,21 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.core:core:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.25")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
